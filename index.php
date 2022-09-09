db7bd758ea3b6e2cdb0de9a171f4ed8d5f69dd9e
Fernweh99 <daniele.dimento@gmail.com>
2022-09-09 16:00:54 +0200

    set public function construct

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ class Movie
   public $description;
   public $relase_date;
   public $genre;
-}
 
-?>
+  public function __construct( $title, $description, $relase_date, $genre ) {
+    $this->title = $title;
+    $this->description = $description;
+    $this->relase_date = $relase_date;
+    $this->genre = $genre;
+  }
+}
